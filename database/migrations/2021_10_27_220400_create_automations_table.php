7f6e2d692f0023ae1633725e801ca47baa2db0f6
Ross Money <5647850+rossmoneyfreelancer@users.noreply.github.com>
2021-10-27 22:09:44 +0100

    create automations table

--- database/migrations/2021_10_27_220400_create_automations_table.php
+++ database/migrations/2021_10_27_220400_create_automations_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSpendingCategoriesTable extends Migration
+class CreateAutomationsTable extends Migration
 {
     public function up()
     {
-        Schema::create('spending_categories', function (Blueprint $table) {
+        Schema::create('automations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->tinyInteger('recurrent');
+            $table->string('description');
+            $table->integer('value');
         });
     }
 }
